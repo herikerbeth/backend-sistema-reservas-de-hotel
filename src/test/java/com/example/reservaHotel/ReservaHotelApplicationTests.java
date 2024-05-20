e9b5ea208ab38f7a8324ee17fffba2ffab9cb2cb
herikerbeth <herik.docarmo@hotmail.com>
2024-05-20 11:24:55 -0300

    first commit

---
+++ src/test/java/com/example/reservaHotel/ReservaHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservaHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
